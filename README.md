dab1b9187f32ffade2d509dad591567007b9628f
Nguyen Hoang Quan <quanjenkey@gmail.com>
2021-06-18 08:03:43 -0700

    add README and fix bugs

---
+++ README.md
@@ -0,0 +1,35 @@
+﻿# ViT - Vision Transformer
+
+This is an implementation of ViT - Vision Transformer by Google Research Team through the paper [**"An Image is Worth 16x16 Words: Transformers for Image Recognition at Scale"**](https://arxiv.org/abs/2010.11929)
+
+
+## ViT Architecture
+![Architecture of Vision Transformer](https://neurohive.io/wp-content/uploads/2020/10/rsz_cov.png)
+
+## Configs
+You can config the network by yourself through the `config.txt` file
+
+```
+64 		#batch_size
+50 		#epoch
+0.001 	#learning_rate
+0.7 	#gamma
+256 	#img_size
+16 		#patch_size
+100		#num_class
+768		#d_model
+12		#n_head
+12		#n_layers
+3072	#d_mlp
+3		#channels
+0.	  	#dropout
+cls		#pool
+```
+
+## Training
+Currently, you can only train this model on CIFAR-100 with the following commands:
+
+`> pip3 install einops`
+`> git clone https://github.com/quanmario0311/ViT_PyTorch.git`
+`> cd ViT_PyTorch`
+`> python3 train.py`
--- train.py
+++ train.py
@@ -35,29 +35,29 @@ def train(model, train_loader, val_loader, criterion, optimizer, scheduler, epoc
             acc = (output.argmax(dim=1) == label).float().mean()
             epoch_accuracy += acc / len(train_loader)
             epoch_loss += loss / len(train_loader)
-
-            data = data.cpu()
-            label = label.cpu()
-
-            epoch_val_accuracy = 0
-            epoch_val_loss = 0
-            for data, label in valid_loader:
-                model.eval()
-                #Load val_data into cuda
-                data = data.to(device)
-                label = label.to(device)
-                #Pass val_data to model
-                val_output = model(data)
-                val_loss = criterion(val_output, label)
-                #Calculate Validation Accuracy
-                acc = (val_output.argmax(dim=1) == label).float().mean()
-                epoch_val_accuracy += acc / len(valid_loader)
-                epoch_val_loss += val_loss / len(valid_loader)
-                data = data.cpu()
-                label = label.cpu()
-        print(
-            f"Epoch : {epoch+1} - loss : {epoch_loss:.4f} - acc: {epoch_accuracy:.4f} - val_loss : {epoch_val_loss:.4f} - val_acc: {epoch_val_accuracy:.4f}\n"
-            )
+            if val_loader is not None:
+                epoch_val_accuracy = 0
+                epoch_val_loss = 0
+                for data, label in valid_loader:
+                    model.eval()
+                    #Load val_data into cuda
+                    data = data.to(device)
+                    label = label.to(device)
+                    #Pass val_data to model
+                    val_output = model(data)
+                    val_loss = criterion(val_output, label)
+                    #Calculate Validation Accuracy
+                    acc = (val_output.argmax(dim=1) == label).float().mean()
+                    epoch_val_accuracy += acc / len(valid_loader)
+                    epoch_val_loss += val_loss / len(valid_loader)
+        if val_loader is not None:
+            print(
+                f"Epoch : {epoch+1} - loss : {epoch_loss:.4f} - acc: {epoch_accuracy:.4f} - val_loss : {epoch_val_loss:.4f} - val_acc: {epoch_val_accuracy:.4f}\n"
+                )
+        else:
+            print(
+                f"Epoch : {epoch+1} - loss : {epoch_loss:.4f} - acc: {epoch_accuracy:.4f}\n"
+                )
 
 
 
@@ -70,7 +70,18 @@ def read_config(config_path: str = "config.txt"):
     return {'batch_size': int(lines[0]),
             'epochs': int(lines[1]),
             'learning_rate': float(lines[2]),
-            'gamma': float(lines[3])}
+            'gamma': float(lines[3]),
+            'img_size': int(lines[4]),
+            'patch_size': int(lines[5]),
+            'num_class': int(lines[6]),
+            'd_model': int(lines[7]),
+            'n_head': int(lines[8]),
+            'n_layers': int(lines[9]),
+            'd_mlp': int(lines[10]),
+            'channels': int(lines[11]),
+            'dropout': float(lines[12]),
+            'pool': lines[13]}
+
     
 
 
@@ -87,15 +98,17 @@ def seed_everything(seed):
 if __name__ == "__main__":
     seed_everything(seed)
     configs = read_config()
+    img_size = configs['img_size']
+
     train_transforms = transforms.Compose([
-        transforms.Resize((256, 256)),
-        transforms.RandomResizedCrop(256),
+        transforms.Resize((img_size, img_size)),
+        transforms.RandomResizedCrop(img_size),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
     ])
 
     test_transforms = transforms.Compose([
-        transforms.Resize((256, 256)),
+        transforms.Resize((img_size, img_size)),
         transforms.ToTensor(),
     ])
 
@@ -109,9 +122,16 @@ if __name__ == "__main__":
     test_loader = DataLoader(dataset = test_data, batch_size=configs['batch_size'], shuffle = True)
     valid_loader = DataLoader(dataset = val_data, batch_size=configs['batch_size'], shuffle = True)
     
-    vision_transformer = ViT(img_size = 256,
-                            patch_size = 16,
-                            num_class = num_class).to(device)
+    vision_transformer = ViT(img_size = configs['img_size'],
+                            patch_size = configs['patch_size'],
+                            num_class = configs['num_class'],
+                            d_model = configs['d_model'],
+                            n_head = configs['n_head'],
+                            n_layers = configs['n_layers'],
+                            d_mlp = configs['d_mlp'],
+                            channels = configs['channels'],
+                            dropout = configs['dropout'],
+                            pool = configs['pool']).to(device)
     #epochs
     epochs = configs['epochs']
     # loss function
@@ -121,4 +141,4 @@ if __name__ == "__main__":
     # scheduler
     scheduler = StepLR(optimizer, step_size=1, gamma=configs['gamma'])
     
-    train(vision_transformer, train_loader, valid_loader, criterion, optimizer, scheduler)
+    train(vision_transformer, train_loader, None, criterion, optimizer, scheduler)
